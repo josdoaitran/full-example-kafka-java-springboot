147497d7e3ee917008dec50890a27d301b364559
Doai Tran <josdoaitran@gmail.com>
2022-09-20 15:05:06 +0700

    update connect db (#2)

--- example-kafka-producer-java-springboot/src/main/java/com/testing4everyone/kafka/user/service/UserServiceApplication.java
+++ example-kafka-producer-java-springboot/src/main/java/com/testing4everyone/kafka/user/service/UserServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootKafkaProducerExampleApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootKafkaProducerExampleApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 }
