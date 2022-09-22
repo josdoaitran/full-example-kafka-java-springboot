d3c486d593c57af1209c2f0f9cd6100f17e63cae
Doai Tran <josdoaitran@gmail.com>
2022-09-22 15:15:25 +0700

    Feature/init connect db (#4)
    * init kafka produce kafka
    
    * update framework example
    
    * update example kafka
    
    * update example kafka

--- example-kafka-producer-java-springboot/src/main/java/com/testing4everyone/kafka/user/service/controller/UserController.java
+++ example-kafka-producer-java-springboot/src/main/java/com/testing4everyone/kafka/user/service/controller/UserController.java
@@ -1,22 +1,34 @@
 package com.testing4everyone.kafka.user.service.controller;
 
 import com.testing4everyone.kafka.user.service.model.User;
+import com.testing4everyone.kafka.user.service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
-@RequestMapping("kafka")
+@RequestMapping("user")
+@CrossOrigin
 public class UserController {
-
+    @Autowired
+    private UserService userService;
     @Autowired
     private KafkaTemplate<String, User> kafkaTemplate;
 
     private static final String TOPIC = "CREATE_NEW_USER_TOPIC";
 
-    @PostMapping("/publish/")
-    public String post(@RequestBody final User user) {
-        kafkaTemplate.send(TOPIC, user);
-        return "Published successfully";
+    @PostMapping("/add")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        User createdUser = userService.saveUser(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(createdUser.getId())
+                .toUri();
+        kafkaTemplate.send(TOPIC, createdUser);
+        return ResponseEntity.created(uri).body(createdUser);
     }
 }
