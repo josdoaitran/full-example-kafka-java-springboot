e545815d4c0eac5f36b86b3480746d91989af8c2
Doai Tran <josdoaitran@gmail.com>
2022-09-23 08:33:51 +0700

    update fraud (#5)

--- example-kafka-consumer-java-springboot/src/main/java/com/testing4everyone/kafka/fraud/service/listener/KafkaConsumer.java
+++ example-kafka-consumer-java-springboot/src/main/java/com/testing4everyone/kafka/fraud/service/listener/KafkaConsumer.java
@@ -1,6 +1,6 @@
-package com.techprimers.kafka.springbootkafkaconsumerexample.listener;
+package com.testing4everyone.kafka.fraud.service.listener;
 
-import com.techprimers.kafka.springbootkafkaconsumerexample.model.User;
+import com.testing4everyone.kafka.fraud.service.model.User;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
