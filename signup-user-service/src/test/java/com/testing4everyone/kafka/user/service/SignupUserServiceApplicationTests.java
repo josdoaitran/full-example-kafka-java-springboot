d4a489e49e13ea832a6e64ff0e5be50118453216
doai.tran <doai.tran@Doai-MacbookPro.local>
2022-09-24 23:14:46 +0700

    refactor

--- signup-user-service/src/test/java/com/testing4everyone/kafka/user/service/SignupUserServiceApplicationTests.java
+++ signup-user-service/src/test/java/com/testing4everyone/kafka/user/service/SignupUserServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class UserServiceApplicationTests {
+public class SignupUserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
