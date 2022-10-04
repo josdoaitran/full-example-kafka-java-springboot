18d119d1e88d77fc85067ca3feb49fc5088b3967
doai.tran <josdoaitran@gmail.com>
2022-10-05 00:06:05 +0700

    example e2e kafka testing

--- example-e2e-test-kafka-testing/src/main/java/ConnectDatabase.java
+++ example-e2e-test-kafka-testing/src/main/java/ConnectDatabase.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class ConnectDatabase {
+import java.sql.*;
+
+public class ConnectDatabase {
+
+
+    public static void deleteDatabase(String url, String userName, String password, String sqlStatement) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(url+ "?user=" + userName + "&password=" + password);
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(sqlStatement);
+
+    }
+
+    public static ResultSet checkDatabase(String url, String userName, String password, String sqlStatement) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(url+ "?user=" + userName + "&password=" + password);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sqlStatement);
+        System.out.println("Printing schema for table: " + resultSet.getMetaData().getTableName(1));
+        int columnCount = resultSet.getMetaData().getColumnCount();
+
+        for (int i = 1; i <= columnCount; i++) {
+            System.out.println(i + " " + resultSet.getMetaData().getColumnName(i));
+        }
+
+        System.out.println("Searching for example user.");
+        boolean exampleUserFound = false;
+
+        return resultSet;
+    }
 }
